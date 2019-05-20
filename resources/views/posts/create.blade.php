74fcd089b5f5353c4369eb8fc546aabdedbc4ae0
A.Y Seth <aysait101@gmail.com>
2019-05-20 09:21:48 +0100

    setting create.blade markup for mini app

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+<form method="post" action="/posts">
+
+	<input type="text" name="title" placeholder="Enter title">
+	<input type="submit" name="submit">
+
+</form>
+
+
+@section('footer')
