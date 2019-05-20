8412a425783f8cce11ea29c8504bc8a9cb7aa295
A.Y Seth <aysait101@gmail.com>
2019-05-20 10:32:35 +0100

    Edit view $ token for mini app

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -3,9 +3,15 @@
 
 @section('content')
 
-<form method="post" action="/posts">
+<h1>Edit</h1>
 
-	<input type="text" name="title" placeholder="Enter title">
+{{csrf_field()}}
+
+<form method="post" action="/posts/{{$post->id}}">
+
+	<input type="hidden" name="_method" value="PUT">
+
+	<input type="text" name="title" placeholder="Enter title" value="{{$post->title}}">
 	{{csrf_field()}}
 
 	<input type="submit" name="submit">
