dde8d8105ff0c7c484e93a9fcdd31da881aeb097
A.Y Seth <aysait101@gmail.com>
2019-05-21 10:52:07 +0100

    installing package laravelHtml

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,7 +5,8 @@
 
 <h1>CREATE</h1>
 
-<form method="post" action="/posts">
+<!-- <form method="post" action="/posts"> -->
+{!! Form::open() !!}       <!--using package--> 
 
 	<input type="text" name="title" placeholder="Enter title">
 	{{csrf_field()}}
